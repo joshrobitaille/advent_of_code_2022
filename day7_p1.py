b9c386cdda97b66f40ab8ae2be4c1fb6164fca47
joshrobitaille <69158219+joshrobitaille@users.noreply.github.com>
2022-12-13 14:00:49 -0600

    Update day7_p1.py

--- day7_p1.py
+++ day7_p1.py
@@ -1,23 +1,21 @@
-class commands(self, parentDir, childDir, size, commandInput, commandOutput):
-    def __init__(self):
+class Command:
+    def __init__(self, commandInput, commandOutput):
+        self.commandInput = commandInput
+        self.commandOutput = commandOutput
+        
+class Directory:
+    def __init__(self, ParentDir, childDir, size):
         self.parentDir = parentDir
         self.childDir = childDir
         self.size = size
-        self.commandInput = commandInput
-        self.commandOutput = commandOutput
 
 with open('input.txt','r') as f:
     input = [l.strip() for l in f]
-    
-# for c in commands:
-#     if c.__contains__('$'):
-#         commands[f'{key}'] = value
-#         key = ''
-#         value.clear()
-#         key += c.strip()
-#         x += 1
-#     else:
-#         value.append(c.strip())
+
+x = 1    
+for c in input:
+    if c.__contains__('$'):
+        f'command{x}' = Command()
 
 print(commands)
 
